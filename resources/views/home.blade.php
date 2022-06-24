12811167fa3f4e6825ba4b559d68a7069084c3b1
Gabry17 <gbianchi528@gmail.com>
2022-06-24 16:39:13 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('main_content')
+<h1>ciao</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="top">HEADER TOP</div>
+    <div class="bottom">HEADER BOTTOM</div>
+    <div class="banner">HEADER BANNER</div>
+</header>
