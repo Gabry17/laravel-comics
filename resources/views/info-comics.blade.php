5718bdaffb8a6e981d68e70ba49144fdbaee6c89
Gabry17 <gbianchi528@gmail.com>
2022-06-27 16:57:24 +0200

    aggiunto stile al info-comics

--- resources/views/info-comics.blade.php
+++ resources/views/info-comics.blade.php
@@ -1,5 +1,62 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
 @section('main_content')
-    ciao
+    <div class="single-cont-top">
+        <div class="single-top">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+        </div>
+    </div>
+
+    <div class="single-cont-center">
+        <div class="single-center">
+            <div class="left">
+                <h2>title</h2>
+                <div class="sale">
+                    <div class="price">
+                        <p>price</p>
+                        <p>available</p>
+                    </div>
+                    <div class="btn">
+                        Check availability
+                    </div>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet eum fuga enim nisi quia libero magnam sequi laboriosam cum totam, consectetur placeat, doloribus, modi eos labore. Magnam consequatur accusamus ad?</p>
+            </div>
+            <div class="right">
+                <h3>ADVERTISEMENT</h3>
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="single-cont-bottom">
+        <div class="single-bottom">
+            <div class="talent">
+                <h3>Talent</h3>
+                <div class="art">
+                    <p>Art by: </p>
+                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores unde expedita quas suscipit, facilis quia, excepturi, aliquam saepe dicta impedit itaque! Enim voluptatibus minima dolorum alias aliquid quae porro dolorem.</span>
+                </div>
+                <div class="written">
+                    <p>Written by:</p>
+                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores unde expedita quas suscipit, facilis quia, excepturi, aliquam saepe dicta impedit itaque! Enim voluptatibus minima dolorum alias aliquid quae porro dolorem.</span>
+                </div>
+            </div>
+            <div class="specs">
+                <h3>Specs</h3>
+                <div class="series">
+                    <p>Series:</p>
+                    <p>ACTION COMICS</p>
+                </div>
+                <div class="price">
+                    <p>U.S. Price:</p>
+                    <p>19</p>
+                </div>
+                <div class="sale-date">
+                    <p>On Sale Date:</p>
+                    <p>oct</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
